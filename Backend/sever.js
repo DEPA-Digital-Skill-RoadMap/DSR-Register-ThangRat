0a9f9e6499976b83c4f82529c625536d65b11897
Lighthit <poomipatsubinmit@gmail.com>
2025-10-21 14:12:24 +0700

    change api endpoint for maintanence or public

--- Backend/sever.js
+++ Backend/sever.js
@@ -118,7 +118,7 @@
     }
   });
 
+  
 
-
-
-  app.listen(3000,"0.0.0.0",() => console.log("✅ Server running on http://localhost:3000"));
+  
+  app.listen(3000,"localhost",() => console.log("✅ Server running on http://localhost:3000"));
